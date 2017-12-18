1c292cc12877c0f781b276152cb1e942fe2f6f2a
fnobakht <fnobakht@sfu.ca>
2017-12-17 17:43:37 -0800

    Update app.js to add express session, validator, passport

--- dating/app.js
+++ dating/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dating');
 
 var app = express();
 
@@ -21,6 +25,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+      var namespace = param.splite('.')
+          , root = namespace.shift()
+          , formParam  = root;
+
+      while(namespace.lenth){
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return{
+        param : formParam,
+        msg : msg,
+        value : value
+      };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+//Global Vars
+app.use(function (req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
