5715fcfe8b8fb26f222bd1cff21d5a8a48f7a0fd
fnobakht <fnobakht@sfu.ca>
2017-12-18 20:23:47 -0800

    Update user schema and add two functions to find user by username and id

--- dating/models/user.js
+++ dating/models/user.js
@@ -1,24 +1,40 @@
-var mongoose = require('mpngoose');
+var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
-mongoose.connect('mongodb://localhost/dating_db');
-
-var db = mongoose.connection;
 
 var UserSchema = mongoose.Schema({
+    first_name: {type: String},
+    last_name: {type: String},
     username:{type: String, index: true},
     password:{type: String},
     email:{type: String},
-    name: {type: String}
+    date_of_birth:{type: Date}
 });
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    bcrypt.getSalt(10, function(err, salt){
+    bcrypt.genSalt(10, function(err, salt){
         bcrypt.hash(newUser.password, salt, function(err, hash){
             newUser.password = hash;
             newUser.save(callback);
         });
     });
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    var query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch){
+       if(err) throw err;
+       callback(null, isMatch);
+    });
+
 }
