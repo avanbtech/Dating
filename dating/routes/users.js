9eeb143c89f95126198836d5355177b7f0d99302
fnobakht <fnobakht@sfu.ca>
2017-12-19 15:24:40 -0800

    Clean up users.js in routes directory and move functions to controller

--- dating/routes/users.js
+++ dating/routes/users.js
@@ -1,108 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var userController = require('../controller/userController');
 
-// Register
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
 
-// Login
-router.get('/login', function(req, res){
-  res.render('login');
-});
+// Register get
+router.get('/register', userController.user_register_get);
 
-// Register User
-router.post('/register', function(req, res, next) {
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var dateOfBirth = req.body.birthday;
-    var email = req.body.email;
-    var username = req.body.username;
-    var password = req.body.password;
-    var password2 = req.body.password2;
-    console.log(req.body);
+//Register post
+router.post('/register', userController.user_register_post);
 
-    // Validation
-    req.checkBody('firstName', 'First name is required').notEmpty();
-    req.checkBody('lastName', 'Last name is required').notEmpty();
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Email is not valid').isEmail();
-    req.checkBody('birthday', 'Date of birth is required').optional({ checkFalsy: true }).isISO8601();
-    req.checkBody('birthday', 'Date of birth is required').notEmpty();
-    req.checkBody('username', 'Username is required').notEmpty();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('password2', 'Password do not match').equals(req.body.password);
+// Login get
+router.get('/login', userController.user_login_get);
 
-    var errors = req.validationErrors();
+// Login post
+router.post('/login', userController.user_login_post);
 
-    if(errors){
-      res.render('register',{errors:errors})
-    }else{
-        var newUser = new User({
-            first_name: firstName,
-            last_name: lastName,
-            username: username,
-            email: email,
-            password: password,
-            date_of_birth: dateOfBirth
-        });
-        User.createUser(newUser, function(err, user){
-            if(err) throw err;
-            console.log(user);
-        });
-        req.flash('success_msg', 'You are registered and can now login');
-        res.redirect('/users/login');
-    }
-});
+// Logout
+router.get('/logout', userController.user_logout_get);
 
-var passport = require('passport')
-    , LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.getUserByUsername(username, function(err, user){
-            if(err) throw err;
-            if(!user){
-                return done(null, false, {message: 'Unknown User'});
-            }
-
-            User.comparePassword(password, user.password, function(err, isMatch){
-               if(err) throw err;
-               if(isMatch){
-                   return done(null, user);
-               }else{
-                   return done(null, false, {message: 'Invalid password'});
-               }
-            });
-        });
-    }));
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.getUserById(id, function(err, user) {
-        done(err, user);
-    });
-});
-
-router.post('/login',
-    passport.authenticate('local', {successRedirect:'/', failureRedirect: '/users/login',failureFalash: true }),
-    function(req, res) {
-    res.redirect('/');
-        // If this function gets called, authentication was successful.
-        // `req.user` contains the authenticated user.
-        res.redirect('/users/' + req.user.username);
-    });
-
-router.get('/logout', function(req, res){
-    req.logout();
-    req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login');
-});
+// Profile
+router.get('/profile', userController.user_profile_get);
 
 module.exports = router;
