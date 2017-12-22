0b2b5ccd21a6d6d96e4afa2b40265d67d32e7bfb
fnobakht <fnobakht@sfu.ca>
2017-12-21 23:50:00 -0800

    Add separate authentication file that export its function.

--- dating/routes/index.js
+++ dating/routes/index.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var authentication = require('./authentication');
 
 /* GET home page. */
-router.get('/', ensureAuthenticated, function(req, res, next) {
+router.get('/', authentication.isLoggedIn , function(req, res, next) {
   res.render('index');
 });
-
-function ensureAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }else{
-        req.flash('error_msg', 'You need to login to access this page');
-        res.redirect('/users/login');
-    }
-}
 
 module.exports = router;
